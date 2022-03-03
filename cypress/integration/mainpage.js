110ce8b59e5d6a8465bd670e96228106a9209272
dhow <dhow@degentoken.xyz>
2022-03-03 14:26:50 -0700

    move visit to beforeEach

--- cypress/integration/mainpage.js
+++ cypress/integration/mainpage.js
@@ -1,10 +1,15 @@
 describe('Degen Website', () => {
-  it('should load', () => {
-    cy.visit('http://localhost:8000/')
-  })
+  beforeEach(() => {
+    cy.visit('http://localhost:8000/');
+  });
   it('toggle dark mode should work', () => {
     cy.get('body').should('have.css', 'background-color', 'rgb(255, 255, 255)')
     cy.get('[data-test=toggleDarkModeButton]').click();
     cy.get('body').should('have.css', 'background-color', 'rgb(26, 32, 44)')
+  })
+  it('modal should show on Connect Wallet button click', () => {
+    cy.get('[data-test="connectWalletButton"]').click();
+    cy.get('[data-test="myAlgoConnectButton"]')
+    cy.get('[data-test="walletConnectButton"]')
   })
 })
