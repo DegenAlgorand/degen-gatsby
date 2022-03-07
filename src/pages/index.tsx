b95889e50ab2edf64fc5845b2e7763b9b65dcc7b
dhow <dhow@degentoken.xyz>
2022-03-07 15:10:37 -0700

    adding community landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import * as React from "react"
 import Layout from "../layout";
-import LaunchSmartContractsHero from "../components/launchSmartContractsHero"
+import BuildingCommunity from "../components/hero-sections/buildingCommunity";
 
 // markup
 const IndexPage = () => {
   return (
     <Layout>
-      <LaunchSmartContractsHero />
+      <BuildingCommunity />
     </Layout>
   )
 }
